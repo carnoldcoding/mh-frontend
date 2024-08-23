0677b8d4a51bdb9016ff2434700f48de782010cf
Logia <carnoldcoding@gmail.com>
2024-08-23 14:34:36 -0400

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/mh-frontend/',
+  server: {
+    hmr: true, // Hot Module Replacement is enabled by default
+    host: '0.0.0.0', // Allows access from any IP address
+    port: 3000, // Or your chosen port
+  },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "@/styles/variables.scss";`
+        }
+      }
+    },
+    build: {
+      outDir: 'dist',
+      rollupOptions: {
+        input: {
+          main: './index.html'
+        }
+      }
+    }
+  });
